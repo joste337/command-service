55dfa60a2f4b96061518f304b5c6ce57ddc5d010
Jörg Stenger <joerg.stenger@exozet.com>
2018-05-31 13:49:19 +0200

    initial commit

---
+++ src/main/java/de/jos/service/command/commandservice/CommandServiceApplication.java
@@ -0,0 +1,12 @@
+package de.jos.service.command.commandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandServiceApplication.class, args);
+	}
+}
